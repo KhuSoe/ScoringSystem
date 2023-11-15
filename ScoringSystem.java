f9ae96eb1743f313a52012a2428da6a54c846be3
KhuSoe <143951063+KhuSoe@users.noreply.github.com>
2023-11-15 20:19:24 +0630

    Update ScoringSystem.java

--- ScoringSystem.java
+++ ScoringSystem.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class ScoringSystem {
     private static final Scanner scan = new Scanner(System.in);
-
+///updated////
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
